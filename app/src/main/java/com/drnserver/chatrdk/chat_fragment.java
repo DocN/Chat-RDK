d91c5ba980d83ae01ab5bf7548048da7bfca7e0e
DrN <DrN@DESKTOP-GORGVHP>
2018-02-11 01:08:23 -0800

    02/11 revision

---
+++ app/src/main/java/com/drnserver/chatrdk/chat_fragment.java
@@ -0,0 +1,13 @@
+package com.drnserver.chatrdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chat_fragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_fragment);
+    }
+}
