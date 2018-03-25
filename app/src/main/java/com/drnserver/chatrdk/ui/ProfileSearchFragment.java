418b1f24562fe0a5fec6f860967db971d7320775
Steven Chang <steevochang@gmail.com>
2018-03-24 19:51:07 -0700

    Added image upload

--- app/src/main/java/com/drnserver/chatrdk/ui/ProfileSearchFragment.java
+++ app/src/main/java/com/drnserver/chatrdk/ui/ProfileSearchFragment.java
@@ -1,9 +1,16 @@
 package com.drnserver.chatrdk.ui;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +20,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.drnserver.chatrdk.DialogUserSearch;
+import com.drnserver.chatrdk.MainActivity;
 import com.drnserver.chatrdk.adaptor.UserSearchAdaptor;
 import com.drnserver.chatrdk.model.UserIndex;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -24,24 +34,34 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.drnserver.chatrdk.R;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
-
-import de.hdodenhof.circleimageview.CircleImageView;
+import java.util.UUID;
 
 public class ProfileSearchFragment extends Fragment implements
         UserSearchAdaptor.OnItemClickListener {
-    private static final String TAG = "Tab1Fragment";
+    private static final String TAG = "Upload";
 
     //Variables - Steven
-    private CircleImageView circleImageView;
+    private FloatingActionButton searchButton;
+    private FloatingActionButton upLoadImage;
     private EditText searchText;
     private RecyclerView resultList;
     private ArrayList<UserIndex> sList;
     private DatabaseReference userIndex;
     private UserSearchAdaptor userSearchAdaptor;
+    private Uri filePath;
+    private String authData;
+    private final int PICK_IMAGE_REQUEST = 71;
+    //Firebase
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
 
     String id = "CZ1Bcon808dHrAJAl6ExxulmgFZ2";
@@ -59,7 +79,11 @@ public class ProfileSearchFragment extends Fragment implements
         //Variable init - Steven
         userIndex = FirebaseDatabase.getInstance().getReference("UserIndex");
         searchText = getView().findViewById(R.id.searchText);
-        circleImageView = getView().findViewById((R.id.searchButton));
+        searchButton = getView().findViewById((R.id.searchButton));
+        upLoadImage = getView().findViewById(R.id.uploadDP);
+        storage = FirebaseStorage.getInstance();
+        authData = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        storageReference = storage.getReference().child("profileImages/" + authData);
 
         //recyclerview - Steven
         resultList = getView().findViewById(R.id.searchList);
@@ -67,11 +91,87 @@ public class ProfileSearchFragment extends Fragment implements
         resultList.setLayoutManager(new LinearLayoutManager(getContext()));
 
         //Listener - Steven
-        circleImageView.setOnClickListener(new View.OnClickListener() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String searchWord = searchText.getText().toString();
                 firebaseUserSearch(searchWord);
 
+            }
+        });
+
+        upLoadImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+    }
+
+    //choose the image to upload
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            uploadImage();
+        }
+    }
+
+    //upload the image to firebase storage
+    private void uploadImage() {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            storageReference.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getContext(), "Uploaded", Toast.LENGTH_SHORT).show();
+                            setProfileImage();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getContext(), "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
+
+    private void setProfileImage() {
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri url) {
+                userIndex.child(authData+"/userSearchImage").setValue(url.toString());
+                //Log.d(TAG, url.toString());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
             }
         });
     }
@@ -129,8 +229,7 @@ public class ProfileSearchFragment extends Fragment implements
             String status = (String) ((DataSnapshot) iterator.next()).getValue();
             String uid = (String) ((DataSnapshot) iterator.next()).getValue();
             UserIndex user = new UserIndex (email, image_url,username, phone, status, uid);
-            if ((user.getUserSearchUID()).equals(FirebaseAuth.getInstance().getCurrentUser()
-                    .getUid())){
+            if ((user.getUserSearchUID()).equals(authData)){
 
             } else {
                 sList.add(user);
