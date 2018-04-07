dd245ed5dbca7b818d3c924d5dec6a4fed750eac
Toaignika <eugene.my88@gmail.com>
2018-04-07 12:24:07 -0700

    Made slider work as intended. Need to fix colors and hook the necessary functionality

---
+++ app/src/main/java/com/drnserver/chatrdk/ui/SwipeControllerActions.java
@@ -0,0 +1,13 @@
+package com.drnserver.chatrdk.ui;
+
+/**
+ * Created by Toa on 4/7/2018.
+ */
+
+public abstract class SwipeControllerActions {
+
+    public void onLeftClicked(int position) {}
+
+    public void onRightClicked(int position) {}
+
+}
